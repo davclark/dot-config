a6ad24f6d4d2e28d8f72a33265cc5f26b2843405
Dav Clark <dav@bead.glass>
2024-03-23 18:36:05 -0700

    Basic styling of wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.font = wezterm.font 'Fira Code'
+
+config.initial_rows = 30
+config.initial_cols = 120
+
+return config
